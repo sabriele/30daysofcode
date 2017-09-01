51f7cbf4274a5a5dfc6c32b008e0ce711aa069f3
Sabrina Tjeng <sabriele@users.noreply.github.com>
2017-09-02 04:13:41 +0800

    Day 04 complete

---
+++ Day 04.js
@@ -0,0 +1,57 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function(data) {
+	input_stdin += data;
+});
+
+process.stdin.on('end', function() {
+	input_stdin_array = input_stdin.split("\n");
+	main();
+});
+
+function readLine() {
+	return input_stdin_array[input_currentline++];
+}
+
+function Person(initialAge) {
+	// Add some more code to run some checks on initialAge
+	var age = initialAge;
+	if (age < 0) {
+		age = 0;
+		process.stdout.write("Age is not valid, setting age to 0.\n");
+	}
+
+	this.amIOld = function() {
+		// Do some computations in here and print out the correct statement to the console
+		if (age < 13) {
+			process.stdout.write("You are young.\n");
+		} else if (age >= 13 && age < 18) {
+			process.stdout.write("You are a teenager.\n");
+		} else {
+			process.stdout.write("You are old.\n");
+		}
+	};
+	this.yearPasses = function() {
+		// Increment the age of the person in here
+		age++;
+	};
+}
+
+function main() {
+	var T = parseInt(readLine());
+	for (i = 0; i < T; i++) {
+		var age = parseInt(readLine());
+		var p = new Person(age);
+		p.amIOld();
+		for (j = 0; j < 3; j++) {
+			p.yearPasses();
+		}
+		p.amIOld();
+		console.log("");
+	}
+}
